e6f37ac25d1990d5c2cfda698d40226381db2954
Noah <noah@noahball.com>
2021-10-03 21:44:35 +1300

    Unique port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 var bodyParser = require('body-parser'); // Parser
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Arrays
 var people = [];
